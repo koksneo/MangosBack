4c708c19ffe161bb3207ee170217ad8c4d2c9bbd
przemratajczak <przemratajczak@wp.pl>
2010-07-11 15:04:25 +0430

    [a07] disable unstuck on battlegrounds

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6629,7 +6629,7 @@ void Spell::EffectStuck(SpellEffectIndex /*eff_idx*/)
     DEBUG_LOG("Spell Effect: Stuck");
     DETAIL_LOG("Player %s (guid %u) used auto-unstuck future at map %u (%f, %f, %f)", pTarget->GetName(), pTarget->GetGUIDLow(), m_caster->GetMapId(), m_caster->GetPositionX(), pTarget->GetPositionY(), pTarget->GetPositionZ());
 
-    if(pTarget->isInFlight())
+    if(pTarget->isInFlight() || pTarget->InBattleGround())
         return;
 
     // homebind location is loaded always

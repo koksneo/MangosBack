4c79444ecb40a8cb70d3bdf73c460382e07ee489
aramil <t_hyzy@wp.pl>
2010-07-29 15:57:56 +0200

    [1] Molten Core by Michalpolko

---
+++ DB_updates/a1_boss_majordomo.sql
@@ -0,0 +1,20 @@
+-- Majordomo Executus and Ragnaros
+-- spawn
+DELETE FROM `creature` WHERE `id` IN (11502, 12018);
+INSERT INTO `creature` VALUES
+(12791715, 11502, 409, 1, 1, 0, 0, 838.385, -831.727, -230.226, 1.71053, 7200, 0, 0, 1099230, 0, 0, 0),
+(12791705, 12018, 409, 1, 1, 0, 731, 750.857, -1180.16, -119.107, 2.86246, 7200, 0, 0, 666200, 0, 0, 0);
+
+-- adds' faction
+UPDATE `creature_template` SET `faction_A` = 1081, `faction_H` = 1081 WHERE `entry` IN (11663, 11664);
+INSERT INTO `creature_ai_scripts` VALUES
+(1166303, 11663, 4, 0, 100, 3, 0, 0, 0, 0, 39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Flamewaker Healer - Call for help"),
+(1166404, 11664, 4, 0, 100, 3, 0, 0, 0, 0, 39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Flamewaker Elite - Call for help");
+
+-- script texts
+DELETE FROM `script_texts` WHERE `entry` = -1409019;
+INSERT INTO `script_texts` (`entry`, `content_default`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+(-1409019, "Very well, $N.", 0, 0, 0, 0, "majordomo SAY_GOSSIP_SUMMON");
+
+-- Emote Yell for Ragnaros (very nervous guy)
+UPDATE `script_texts` SET `emote = 1 WHERE `entry` BETWEEN -1409018 AND -1409009;
--- src/bindings/ScriptDev2/scripts/eastern_kingdoms/molten_core/boss_magmadar.cpp
+++ src/bindings/ScriptDev2/scripts/eastern_kingdoms/molten_core/boss_magmadar.cpp
@@ -22,6 +22,7 @@ SDCategory: Molten Core
 EndScriptData */
 
 #include "precompiled.h"
+#include "molten_core.h"
 
 enum
 {
@@ -36,7 +37,13 @@ enum
 
 struct MANGOS_DLL_DECL boss_magmadarAI : public ScriptedAI
 {
-    boss_magmadarAI(Creature* pCreature) : ScriptedAI(pCreature) {Reset();}
+    boss_magmadarAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    ScriptedInstance* m_pInstance;
 
     uint32 Frenzy_Timer;
     uint32 Panic_Timer;
@@ -49,6 +56,12 @@ struct MANGOS_DLL_DECL boss_magmadarAI : public ScriptedAI
         Lavabomb_Timer = 12000;
 
         m_creature->CastSpell(m_creature,SPELL_MAGMASPIT,true);
+    }
+
+    void JustDied(Unit* pKiller)
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_MAGMADAR, DONE);
     }
 
     void UpdateAI(const uint32 diff)

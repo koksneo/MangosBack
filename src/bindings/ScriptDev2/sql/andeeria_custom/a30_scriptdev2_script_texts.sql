d52ec83b41394d853cf553492a06933a8759c8cc
PanteraPolnocy <NorthPanther@interia.pl>
2010-07-30 00:12:20 +0200

    [a30]* Blackrock Depths - High Justice Grimstone missing... sql file :P Need to be tested, of course.

---
+++ src/bindings/ScriptDev2/sql/andeeria_custom/a30_scriptdev2_script_texts.sql
@@ -0,0 +1,8 @@
+DELETE FROM `script_texts` WHERE `entry` BETWEEN -1000099 AND -1000094;
+INSERT INTO `script_texts` (`entry`,`content_default`,`type`,`comment`) VALUES
+('-1000094', 'You have been sentenced to death for crimes against the Dark Iron nation!', '1', 'High Justice Grimstone 1/6'),
+('-1000095', 'The Sons of Thaurissan shall watch you perish in the Ring of the Law!', '1', 'High Justice Grimstone 2/6'),
+('-1000096', 'Unleash the fury and let it be done!', '1', 'High Justice Grimstone 3/6'),
+('-1000097', 'Haha! I bet you thought you were done!', '1', 'High Justice Grimstone 4/6'),
+('-1000098', 'But your real punishment lies ahead.', '1', 'High Justice Grimstone 5/6'),
+('-1000099', 'Good riddance!', '1', 'High Justice Grimstone 6/6');

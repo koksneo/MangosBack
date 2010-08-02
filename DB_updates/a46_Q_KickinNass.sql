694d2cbd5dab976319ee8457416b077b5c01ef4a
koksneo <koksneo@github.com>
2010-08-02 17:37:18 +0200

    [a46] Support for quest Kickin'Nass and Takin manes

---
+++ DB_updates/a46_Q_KickinNass.sql
@@ -0,0 +1,15 @@
+-- Support for quest Kickin'Nass and Takin manes (12630)
+
+UPDATE creature_template SET
+AIName='EventAI'
+WHERE entry=28519;
+
+DELETE FROM creature WHERE id=28523;
+
+UPDATE creature_template SET
+flags_extra=flags_extra 128
+WHERE entry=28523;
+
+DELETE FROM creature_ai_scripts WHERE creature_id=28519;
+INSERT into creature_ai_scripts VALUES
+(2851901,28519,6, 0, 100, 0, 0, 0, 0, 0, 12, 28523, 0, 30000, 0, 0, 0, 0, 0, 0, 0, 0, 'Withered Troll-summon Nass Target KC Bunny after death');

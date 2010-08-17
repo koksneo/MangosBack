42b7e1e004b43374077934a62c42504d1a075d06
Grandres <grundil@wp.pl>
2010-08-17 16:59:47 +0200

    Typo again xd

--- DB_updates/a61_Q_OnlyOneHope.sql
+++ DB_updates/a61_Q_OnlyOneHope.sql
@@ -1,6 +1,8 @@
 -- Support for quest Only One Hope
 
-UPDATE creature_template SET flags_extra=flags_extra|128 WHERE entry=29647;
+UPDATE gameobject_template SET ScriptName='go_scourge_enclosure' WHERE entry=191548;
+UPDATE creature_template SET flags_extra=flags_extra|128 WHERE entry=29928;
+
 DELETE FROM creature WHERE id=29928;
 INSERT INTO creature VALUES
 (2992801,29928,571,1,1,26656,0,5796.48,-1594.16,235.111,2.16421,300,0,0,11379,0,0,0);

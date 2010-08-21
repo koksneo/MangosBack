b71d3e649726cfa6d3b4a0f7a5a1cdbd57c55216
PanteraPolnocy <NorthPanther@interia.pl>
2010-08-21 21:25:09 +0200

    [a68] Death Comes From High, Grand Theft Palomino, Into the Realm of Shadows - autocomplete disabled

---
+++ src/bindings/ScriptDev2/sql/andeeria_custom/a68_mangos_quest_template.sql
@@ -0,0 +1,2 @@
+-- Death Comes From High, Grand Theft Palomino, Into the Realm of Shadows - autocomplete disabled
+UPDATE `quest_template` SET `Method` = '2' WHERE `entry` IN ('12641','12680','12687');

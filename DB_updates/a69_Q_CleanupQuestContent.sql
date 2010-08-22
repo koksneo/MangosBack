7e8c845a956bccc10eadfc665bca500ada52efeb
Fryzjer <fryzjer@github.com>
2010-08-22 16:17:36 +0200

    [a69] Cleanup quest content. Old Naxxramas quests

---
+++ DB_updates/a69_Q_CleanupQuestContent.sql
@@ -0,0 +1,7 @@
+-- Cleanup for Quest Content
+
+## Old Naxxramas Quests
+
+DELETE FROM creature_questrelation WHERE quest IN (9029,9030,9034,9036,9037,9038,9039,9040,9041,9042);
+DELETE FROM quest_template WHERE entry IN (9029,9030,9034,9036,9037,9038,9039,9040,9041,9042);
+DELETE FROM creature_involvedrelation WHERE quest IN (9029,9030,9034,9036,9037,9038,9039,9040,9041,9042);

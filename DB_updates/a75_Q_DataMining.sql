5e6d2c01a0c91b8697cbd741199fce5a2add6976
koksneo <koksneo@github.com>
2010-08-24 03:31:52 +0200

    [a75] Support for quest Data Mining

---
+++ DB_updates/a75_Q_DataMining.sql
@@ -0,0 +1,10 @@
+-- Data Mining (12860)
+
+UPDATE creature_template SET
+AIName='EventAI'
+WHERE entry=29746;
+
+DELETE from creature_ai_scripts WHERE creature_id=29746;
+INSERT INTO creature_ai_scripts VALUES 
+(2974601,29746,8,0,100,0,55161,-1,0,0,33,29752,6,0,41,6000,0,0,0,0,0,0,'Databank-quest credit after spell hit and despawn(6sec delay)');
+

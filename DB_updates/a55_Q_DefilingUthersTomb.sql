978d2d41b5aa75ecd69b3ba213fecf55a68aa22d
Grandres <grundil@wp.pl>
2010-08-26 20:22:14 +0200

    Another way of handling text in [a55]. It will be more proper

--- DB_updates/a55_Q_DefilingUthersTomb.sql
+++ DB_updates/a55_Q_DefilingUthersTomb.sql
@@ -6,26 +6,28 @@ UPDATE quest_template SET ReqCreatureOrGOId1 =17253 WHERE entry=9444;
 
 -- Uther's ghost (17233)
 
-UPDATe creature_template SET MovementType=0, AIName='EventAI' WHERE entry=17233;
-DELETE FROM creature_ai_scripts WHERE creature_id=17233;
-INSERT INTO creature_ai_scripts VALUES
-(1723301,17233,1,0,100,0,2000,2000,0,0,1,-172331,0,0,0,0,0,0,0,0,0,0,'Ghost od Uther Lightbringer - Say 1'),
-(1723302,17233,1,0,100,0,7000,7000,0,0,1,-172332,0,0,0,0,0,0,0,0,0,0,'Ghost od Uther Lightbringer - Say 2'),
-(1723303,17233,1,0,100,0,12000,12000,0,0,1,-172333,0,0,0,0,0,0,0,0,0,0,'Ghost od Uther Lightbringer - Say 3'),
-(1723304,17233,1,0,100,0,17000,17000,0,0,1,-172334,0,0,0,0,0,0,0,0,0,0,'Ghost od Uther Lightbringer - Say 4');
+UPDATE creature_template SET MovementType=0 WHERE entry=17233;
 
 DELETE FROM event_scripts WHERE id=10561;
 INSERT INTO event_scripts VALUES
 (10561,0,10,17233,21000,0,0,0,0,972.67,-1829.82,81.52,1.05),
 (10561,0,7,9444,0,0,0,0,0,0,0,0,0),
-(10561,0,8,17253,0,0,0,0,0,0,0,0,0);
+(10561,0,8,17253,0,0,0,0,0,0,0,0,0),
+-- Text
+(10561,3,0,17233,30,0x04,2000000200,0,0,0,0,0,0),
+(10561,7,0,17233,30,0x04,2000000201,0,0,0,0,0,0),
+(10561,12,0,17233,30,0x04,2000000202,0,0,0,0,0,0),
+(10561,17,0,17233,30,0x04,2000000203,0,0,0,0,0,0);
 
-DELETE FROM creature_ai_texts WHERE entry IN (-172331,-172332,-172333,-172334,-18541);
+DELETE FROM db_script_string WHERE entry IN (2000000200,2000000201,2000000203,2000000204);
+INSERT INTO db_script_string (entry,content_default) VALUES
+(2000000200,'Why do you do this? Did I somehow wrong you in life?'),
+(2000000201,'Ah, I see it now in your mind. This is the work of one of my former students... Mehlar Dawnblade. It is sad to know that his heart has turned so dark.'),
+(2000000202,'Return to him. Return to Mehlor and tell him that I forgive him and that I understand why he believes what he does.'),
+(2000000203,'I can only hope that he will see the Light and instead turn his energies to restoring once-beautiful Quel\'Thalas.');
+
+DELETE FROM creature_ai_texts WHERE entry=-18541;
 INSERT INTO creature_ai_texts (entry,content_default,type,comment) VALUES
-(-172331,'Why do you do this? Did I somehow wrong you in life?',0,'Ghost of Uther Lightbringer say1'),
-(-172332,'Ah, I see it now in your mind. This is the work of one of my former students... Mehlar Dawnblade. It is sad to know that his heart has turned so dark.',0,'Ghost of Uther Lightbringer say2'),
-(-172333,'Return to him. Return to Mehlor and tell him that I forgive him and that I understand why he believes what he does.',0,'Ghost of Uther Lightbringer say3'),
-(-172334,'I can only hope that he will see the Light and instead turn his energies to restoring once-beautiful Quel\'Thalas.',0,'Ghost of Uther Lightbringer say4'),
 (-18541,'By the Light you will leave this tomb!',0,'High Priest Thel\'danis say1');
 
 -- High Priest Thel'danis (1854)

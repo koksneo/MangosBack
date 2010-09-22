7281f4de3f10ebe2c904f0773b8a1aa8ec85a8e9
koksneo <koksneo@github.com>
2010-09-22 20:29:07 +0200

    [a121] Support for quest Plan B

---
+++ DB_updates/a121_Q_PlanB.sql
@@ -0,0 +1,17 @@
+# Quest: Plan B (11658)
+
+DELETE from spell_scripts WHERE id=45713;
+INSERT into spell_scripts VALUES
+(45713,0,17,34842,1,0,0,0,0,0,0,0,0),
+(45713,1,18,5,0,0,0,0,0,0,0,0,0);
+
+DELETE from npc_spellclick_spells WHERE npc_entry IN(25343,25342);
+INSERT into npc_spellclick_spells VALUES
+(25343,45713,11658,1,11658,1),
+(25342,45713,11658,1,11658,1),
+(25343,45713,11658,1,11658,0),
+(25342,45713,11658,1,11658,0);
+
+UPDATE creature_template SET
+npcflag=npcflag &~1
+where entry IN(25343,25342);

9b354f3459869dc90c6c870b28ea7be12a1b3aa2
Grandres <grundil@wp.pl>
2010-10-03 20:30:58 +0200

    Typo in [a125]

--- DB_updates/mangos/to_update/a125_Q_Abduction.sql
+++ DB_updates/mangos/to_update/a125_Q_Abduction.sql
@@ -1,5 +1,6 @@
 # Quest: Abduction (11590)
 
+UPATE creature_template SET ScriptName='npc_beryl_sorcerer' WHERE entry=25316;
 UPDATE creature_template SET
 minhealth=6387,
 maxhealth=7185,
@@ -7,6 +8,6 @@ minlevel=64,
 maxlevel=70,
 minmana=7031,
 maxmana=7196,
-ScriptName='npc_beryl_sorcerer',
+ScriptName='',
 AIName=''
 WHERE entry=25474;

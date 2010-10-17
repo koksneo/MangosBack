325441fea2f171c54e187a05474d8860e60a0a86
michalpolko <michalpolko@o2.pl>
2010-10-17 02:00:28 +0200

    Typos in [a145] ACID scripts.

--- DB_updates/mangos/to_update/a145_instance_obsidian_sanctum.sql
+++ DB_updates/mangos/to_update/a145_instance_obsidian_sanctum.sql
@@ -136,15 +136,15 @@ UPDATE `creature_template` SET `spell1` = 56911 WHERE `entry` = 30494;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (30680, 30681, 30682, 30453);
 INSERT INTO `creature_ai_scripts` VALUES
 (3068001, 30680, 0, 0, 100, 7, 5000, 10000, 5000, 10000, 11, 13737, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Mortal Strike"),
-(3068002, 30680, 6, 0, 100, 3, 0, 0, 0, 0, 11, 57740, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Devotion Aura (10man)"),
-(3068003, 30680, 6, 0, 100, 5, 0, 0, 0, 0, 11, 58944, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Devotion Aura (25man)"),
-(3068004, 30680, 4, 0, 100, 6, 1, 25, 0, 0, 11, 57742, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Avenging Fury at 25% HP"),
+(3068002, 30680, 4, 0, 100, 3, 0, 0, 0, 0, 11, 57740, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Devotion Aura (10man)"),
+(3068003, 30680, 4, 0, 100, 5, 0, 0, 0, 0, 11, 58944, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Devotion Aura (25man)"),
+(3068004, 30680, 2, 0, 100, 6, 1, 25, 0, 0, 11, 57742, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Avenging Fury at 25% HP"),
 (3068005, 30680, 0, 0, 100, 3, 5000, 10000, 15000, 20000, 11, 57733, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Draconic Rage (10man)"),
 (3068006, 30680, 0, 0, 100, 5, 5000, 10000, 15000, 20000, 11, 58942, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Brood General - Cast Draconic Rage (25man)"),
 
 (3068101, 30681, 0, 0, 100, 3, 5000, 10000, 10000, 15000, 11, 57757, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Rain of Fire (10man)"),
 (3068102, 30681, 0, 0, 100, 5, 5000, 10000, 10000, 15000, 11, 58936, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Rain of Fire (25man)"),
-(3068103, 30681, 4, 0, 100, 7, 1, 50, 0, 0, 11, 57753, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Conjure Flame Orb at 50% HP"),
+(3068103, 30681, 2, 0, 100, 7, 1, 50, 0, 0, 11, 57753, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Conjure Flame Orb at 50% HP"),
 (3068104, 30681, 0, 0, 100, 3, 5000, 10000, 10000, 15000, 11, 39529, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Flame Shock (10man)"),
 (3068105, 30681, 0, 0, 100, 5, 5000, 10000, 10000, 15000, 11, 58940, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Blaze Mistress - Cast Flame Shock (25man)"),
 
@@ -154,7 +154,7 @@ INSERT INTO `creature_ai_scripts` VALUES
 (3045301, 30453, 0, 0, 100, 3, 5000, 10000, 10000, 15000, 11, 57728, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Sanctum Guardian - Cast Shockwave (10man)"),
 (3045302, 30453, 0, 0, 100, 5, 5000, 10000, 10000, 15000, 11, 58947, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Sanctum Guardian - Cast Shockwave (25man)"),
 (3045303, 30453, 0, 0, 100, 6, 5000, 7000, 0, 0, 11, 58948, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Sanctum Guardian - Cast Curse of Mending"),
-(3045304, 30453, 4, 0, 100, 6, 1, 25, 0, 0, 11, 53801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Sanctum Guardian - Cast Frenzy at 25% HP");
+(3045304, 30453, 2, 0, 100, 6, 1, 25, 0, 0, 11, 53801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Onyx Sanctum Guardian - Cast Frenzy at 25% HP");
 
 -- Drakes' templates - instance binding dependent on instance script; set lootid.
 UPDATE `creature_template` SET `lootid` = `entry`, `flags_extra` = `flags_extra`&~1 WHERE `entry` IN (30449, 30451, 30452, 31535, 31520, 31534);

96b9c7b01b7057cc54b693eb8bf39ebcaa9ee7f4
michalpolko <michalpolko@o2.pl>
2010-10-20 01:56:59 +0200

    Just some typos in [a145].

--- DB_updates/mangos/to_update/a145_instance_obsidian_sanctum.sql
+++ DB_updates/mangos/to_update/a145_instance_obsidian_sanctum.sql
@@ -3,16 +3,16 @@
 -- ################################################################
 
 -- ScriptNames
-UPDATE `creature_template` SET `ScriptName = "boss_sartharion" WHERE entry = 28860;
-UPDATE `creature_template` SET `ScriptName = "mob_vesperon" WHERE entry = 30449;
-UPDATE `creature_template` SET `ScriptName = "mob_shadron" WHERE entry = 30451;
-UPDATE `creature_template` SET `ScriptName = "mob_tenebron" WHERE entry = 30452;
-UPDATE `creature_template` SET `ScriptName = "mob_acolyte_of_shadron" WHERE entry = 31218;
-UPDATE `creature_template` SET `ScriptName = "mob_acolyte_of_vesperon" WHERE entry = 31219;
-UPDATE `creature_template` SET `ScriptName = "mob_twilight_eggs" WHERE entry = 30882;
-UPDATE `creature_template` SET `ScriptName = "mob_twilight_whelp" WHERE entry IN (30890, 31214);
-UPDATE `creature_template` SET `ScriptName = "mob_fire_cyclone" WHERE entry = 30648;
-UPDATE `creature_template` SET `ScriptName = "mob_flame_tsunami" WHERE entry = 30616;
+UPDATE `creature_template` SET `ScriptName` = "boss_sartharion" WHERE `entry` = 28860;
+UPDATE `creature_template` SET `ScriptName` = "mob_vesperon" WHERE `entry` = 30449;
+UPDATE `creature_template` SET `ScriptName` = "mob_shadron" WHERE `entry` = 30451;
+UPDATE `creature_template` SET `ScriptName` = "mob_tenebron" WHERE `entry` = 30452;
+UPDATE `creature_template` SET `ScriptName` = "mob_acolyte_of_shadron" WHERE `entry` = 31218;
+UPDATE `creature_template` SET `ScriptName` = "mob_acolyte_of_vesperon" WHERE `entry` = 31219;
+UPDATE `creature_template` SET `ScriptName` = "mob_twilight_eggs" WHERE `entry` = 30882;
+UPDATE `creature_template` SET `ScriptName` = "mob_twilight_whelp" WHERE `entry` IN (30890, 31214);
+UPDATE `creature_template` SET `ScriptName` = "mob_fire_cyclone" WHERE `entry` = 30648;
+UPDATE `creature_template` SET `ScriptName` = "mob_flame_tsunami" WHERE `entry` = 30616;
 
 -- Shadow Fissure
 UPDATE `creature_template` SET `AIName` = "EventAI" WHERE entry = 30641;

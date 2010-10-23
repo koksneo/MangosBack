3cdefcba7b6f2dd63fe69aa87266ef3fd51b6bd0
Kurek <kurek@github.com>
2010-10-23 13:06:11 +0200

    [a158] Decimation - proc also from Soul Fire.

---
+++ DB_updates/mangos/to_update/a158_spell_Decimation.sql
@@ -0,0 +1,2 @@
+-- Proc also from Soul Fire
+UPDATE `spell_proc_event` SET `SpellFamilyMaskB0` = `SpellFamilyMaskB0`|128, `SpellFamilyMaskB1` = `SpellFamilyMaskB1`|128, `SpellFamilyMaskB2` = `SpellFamilyMaskB2`|128 WHERE `entry` = 63156;

e52744d90fe32c699ce60107f38f2a4a8f81d708
Grandres <grundil@wp.pl>
2010-10-24 15:18:44 +0200

    [a163] Crashfix. Provided by Lightguard and insider42. Need to test.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1759,7 +1759,7 @@ bool InstanceMap::Add(Player *player)
                         GetInstanceSave()->GetMapId(), GetInstanceSave()->GetInstanceId(),
                         GetInstanceSave()->GetDifficulty(), GetInstanceSave()->GetPlayerCount(),
                         GetInstanceSave()->GetGroupCount(), GetInstanceSave()->CanReset());
-                    MANGOS_ASSERT(false);
+                    player->RepopAtGraveyard();
                 }
             }
             else
@@ -1809,7 +1809,7 @@ bool InstanceMap::Add(Player *player)
                                 sLog.outError("GroupBind save players: %d, group count: %d", groupBind->save->GetPlayerCount(), groupBind->save->GetGroupCount());
                             else
                                 sLog.outError("GroupBind save NULL");
-                            MANGOS_ASSERT(false);
+                            player->RepopAtGraveyard();
                         }
                         // if the group/leader is permanently bound to the instance
                         // players also become permanently bound when they enter
@@ -1934,7 +1934,7 @@ bool InstanceMap::Reset(uint8 method)
 
     if(HavePlayers())
     {
-        if(method == INSTANCE_RESET_ALL)
+        if(method == INSTANCE_RESET_ALL || method == INSTANCE_RESET_CHANGE_DIFFICULTY)
         {
             // notify the players to leave the instance so it can be reset
             for(MapRefManager::iterator itr = m_mapRefManager.begin(); itr != m_mapRefManager.end(); ++itr)

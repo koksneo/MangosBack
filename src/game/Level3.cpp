60f42ed876e0e8ddfc27332e34c68c0f51a5618f
Insider <insider42rus@yandex.ru>
2010-10-31 20:37:16 +0600

    Added new version of [patch 123]

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -5695,6 +5695,8 @@ bool ChatHandler::HandleGMFlyCommand(char* args)
     if (!target)
         target = m_session->GetPlayer();
 
+    target->SetCanFly(value ? true : false);
+
     WorldPacket data(12);
     data.SetOpcode(value ? SMSG_MOVE_SET_CAN_FLY : SMSG_MOVE_UNSET_CAN_FLY);
     data << target->GetPackGUID();

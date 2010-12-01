cc6ce25a8500b0bbbf09e7033bfd104670798c03
michalpolko <michalpolko@o2.pl>
2010-12-01 17:14:18 +0100

    [a204] Reverted forgotten dev-changes from [a202].

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1765,7 +1765,7 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
         {
             if (EffectChainTarget <= 1)
             {
-                if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(pUnitTarget, m_spellInfo))
+                if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), m_spellInfo))
                 {
                     m_targets.setUnitTarget(pUnitTarget);
                     targetUnitMap.push_back(pUnitTarget);
@@ -2383,7 +2383,7 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 }
                 else
                 {
-                    if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(pUnitTarget, m_spellInfo))
+                    if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), m_spellInfo))
                     {
                         m_targets.setUnitTarget(pUnitTarget);
                         targetUnitMap.push_back(pUnitTarget);
@@ -2413,7 +2413,7 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             break;
         case TARGET_SINGLE_ENEMY:
         {
-            if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(pUnitTarget, m_spellInfo))
+            if(Unit *pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), m_spellInfo))
             {
                 m_targets.setUnitTarget(pUnitTarget);
                 targetUnitMap.push_back(pUnitTarget);

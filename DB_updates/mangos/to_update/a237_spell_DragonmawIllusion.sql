6a7d9f69c6946550f6cc5a3919ae7b6967dcaf0f
michalpolko <michalpolko@o2.pl>
2010-12-08 16:31:33 +0100

    Last commit - should work with both genders of course ;)

--- DB_updates/mangos/to_update/a237_spell_DragonmawIllusion.sql
+++ DB_updates/mangos/to_update/a237_spell_DragonmawIllusion.sql
@@ -1,3 +1,3 @@
 DELETE FROM `spell_area` WHERE `spell` = 40214 AND `area` = 3965; 
-INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `autocast`) VALUES
-('40214', '3965', '11013', '1', '1'); 
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `gender`, `autocast`) VALUES
+('40214', '3965', '11013', '1', '2', '1'); 

301fa45c5d280ae2df77525f7afb63e1b6049163
michalpolko <michalpolko@o2.pl>
2010-12-12 19:27:05 +0100

    [a245] Missing SQL for Halls Of Stone.

--- DB_updates/mangos/to_update/a245_instance_HallsOfStone.sql
+++ DB_updates/mangos/to_update/a245_instance_HallsOfStone.sql
@@ -6,6 +6,8 @@
 UPDATE creature_template SET ScriptName = 'boss_maiden_of_grief' WHERE entry = 27975;
 UPDATE creature_template SET ScriptName = 'boss_krystallus' WHERE entry = 27977;
 UPDATE creature_template SET ScriptName = 'boss_sjonnir' WHERE entry = 27978;
+UPDATE creature_template SET ScriptName='mob_tribuna_controller' WHERE entry=28234;
+UPDATE creature_template SET ScriptName='npc_brann_hos' WHERE entry=28070;
 
 -- Boss Sjonnir and adds
 UPDATE creature_template SET AIName='EventAI' WHERE entry IN (27979,27982);

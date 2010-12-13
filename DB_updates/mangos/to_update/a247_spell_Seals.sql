ecc3e69b187c178f6ba190ad6f14467fd38a6948
michalpolko <michalpolko@o2.pl>
2010-12-13 23:07:13 +0100

    [a247] Fixed typo in a236.

--- DB_updates/mangos/to_update/a247_spell_Seals.sql
+++ DB_updates/mangos/to_update/a247_spell_Seals.sql
@@ -1,7 +1,7 @@
 -- Paladin Seals proc
 -- should be ppm but doesn't work, must use workaround with cooldown (similiar effect)
 
-DELETE FROM `spell_proc_event` WHERE `entry` IN (20185, 20186);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (20164, 20165, 20166);
 INSERT INTO `spell_proc_event` VALUES
 (20166, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4), -- Seal of Wisdom
 (20165, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3), -- Seal of Light
